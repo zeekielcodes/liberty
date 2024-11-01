286ca3e6f8f730f82eb93bc26218bde21ab3b8d9
Zeek <ezekielabolade@gmail.com>
2024-11-01 13:44:57 +0100

    One last tiny modification

--- components/TimeUp.tsx
+++ components/TimeUp.tsx
@@ -7,7 +7,7 @@ import { MdOutlineTimerOff } from "react-icons/md";
 export default function TimeUp() {
 	const { resetTime } = useTimeTracker();
 	return (
-		<section className="bg-white rounded-xl p-4 md:p-6 w-full md:w-4/5 lg:w-3/5 flex flex-col gap-22 justify-center items-center">
+		<section className="bg-white min-h-[60vh] rounded-xl p-4 md:p-6 w-full md:w-4/5 lg:w-3/5 flex flex-col gap-22 justify-center items-center">
 			<MdOutlineTimerOff className="text-[80px] text-red-500" />
 			<h1 className="text-base md:text-lg my-3">Timeout!</h1>
 			<button
